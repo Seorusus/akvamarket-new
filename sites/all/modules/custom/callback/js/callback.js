c6ba1f5f066c18814ed3183b08697680dbdb22a6
seorusus <seorusus@gmail.com>
2018-05-09 15:14:31 +0300

    Added resolve task #2

---
+++ sites/all/modules/custom/callback/js/callback.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function () {
+        jQuery(document).ready(function () {
+            
+            $("#edit-phone").mask("+99 (999) 999-99-99");
+            
+         });
+    });
+})(jQuery);
